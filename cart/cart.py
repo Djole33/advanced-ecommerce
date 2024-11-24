8605611484053d8cc9adb73844552c8ab2956afd
WebWrap <149431380+Djole33@users.noreply.github.com>
2024-11-24 14:02:29 +0100

    add and update cart functionality

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from main.models import Product
+
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -8,3 +10,23 @@ class Cart():
             cart = self.session['session_key'] = {}
 
         self.cart = cart
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
+    
+    def cart_prods(self):
+        product_ids = self.cart.keys()
+
+        products = Product.objects.filter(id__in=product_ids)
+
+        return products
